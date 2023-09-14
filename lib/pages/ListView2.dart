b90d63c850f642a39faa80bec0c4ccea8acadc7a
Rafael <roboticra@gmail.com>
2023-09-13 22:07:06 -0500

    cambios pequeños a la lista 2

--- lib/pages/ListView2.dart
+++ lib/pages/ListView2.dart
@@ -17,13 +17,15 @@ class ListView2Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Ejemplo de ListView 2"),
+        elevation: 0,
+        backgroundColor: Colors.black,
       ),
       body: ListView.separated(
           itemCount: items.length,
           itemBuilder: (context, index) => ListTile(
               onTap: () {},
               title: Text(items[index]),
-              leading: const Icon(Icons.person),
+              leading: const Icon(Icons.person, color: Colors.red),
               trailing: const Icon(Icons.arrow_forward_sharp)),
           separatorBuilder: (__, _) => const Divider()),
     );
