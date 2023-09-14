6ebc498506ac993672c583d243fd9d9e17a8ee53
Rafael <roboticra@gmail.com>
2023-09-13 21:03:36 -0500

    se quito la franja de debug en la aplicacion

--- lib/App.dart
+++ lib/App.dart
@@ -4,6 +4,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: const Text("Aplicación Android"),
