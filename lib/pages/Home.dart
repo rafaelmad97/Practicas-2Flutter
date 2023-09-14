84ed4ef3067e8d1787f9845a1bb30317bde74b2b
Rafael <roboticra@gmail.com>
2023-09-13 22:45:19 -0500

    se implementaron las primeras opciones en el homepage

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final opciones = ["Tipos de listas"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My App Android"),
       ),
-      body: const Text("Inicio"),
+      body: ListView.separated(
+          itemBuilder: (__, index) => ListTile(
+                title: Text(opciones[index]),
+                onTap: () {},
+                trailing: const Icon(Icons.open_in_new),
+              ),
+          separatorBuilder: (__, index) => const Divider(),
+          itemCount: opciones.length),
     );
   }
 }
